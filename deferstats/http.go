b3bebd1c621fe6a5e065b569fcadc2f348ef3335
Ian Eyberg <ian.eyberg@tobi.com>
2014-12-31 16:04:59 -0800

    time panics as well

--- deferstats/http.go
+++ deferstats/http.go
@@ -9,30 +9,37 @@ import (
 // curlist holds an array of DeferHTTPs (uri && latency)
 var curlist []DeferHTTP
 
+func appendHTTP(startTime time.Time, path string) {
+	endTime := time.Now()
+
+	t := int(((endTime.Sub(startTime)).Nanoseconds() / 1000000))
+
+	dh := DeferHTTP{
+		Path: path,
+		Time: t,
+	}
+
+	curlist = append(curlist, dh)
+}
+
 // HTTPHandler wraps a http handler and captures the latency of each
 // request
 func HTTPHandler(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+
 		defer func() {
 			if err := recover(); err != nil {
 				// hack
 				deferclient.Token = Token
 				deferclient.Prep(err)
+
+				appendHTTP(startTime, r.URL.Path)
 			}
 		}()
 
-		startTime := time.Now()
 		f(w, r)
-		endTime := time.Now()
 
-		t := int(((endTime.Sub(startTime)).Nanoseconds() / 1000000))
-
-		dh := DeferHTTP{
-			Path: r.URL.Path,
-			Time: t,
-		}
-
-		curlist = append(curlist, dh)
-
+		appendHTTP(startTime, r.URL.Path)
 	}
 }
