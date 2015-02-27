2c9fe8440a375c42aa8ffe6e6acfeb1e66125fc8
Ian Eyberg <ian@deferpanic.com>
2015-02-27 13:51:01 +0530

    tracing

--- deferstats/http.go
+++ deferstats/http.go
@@ -3,26 +3,42 @@ package deferstats
 import (
 	"github.com/deferpanic/deferclient/deferclient"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
 // curlist holds an array of DeferHTTPs (uri && latency)
 var curlist []DeferHTTP
 
+var latencyThreshold = 200
+
 // appendHTTP adds a new http request to the list
-func appendHTTP(startTime time.Time, path string, status_code int) {
+func appendHTTP(startTime time.Time, path string, status_code int, span_id int64, parent_span_id int64) {
 	endTime := time.Now()
 
 	t := int(((endTime.Sub(startTime)).Nanoseconds() / 1000000))
 
-	dh := DeferHTTP{
-		Path:       path,
-		Time:       t,
-		StatusCode: status_code,
-	}
+	// only log over t
+	if t > latencyThreshold {
 
-	curlist = append(curlist, dh)
+		dh := DeferHTTP{
+			Path:         path,
+			Time:         t,
+			StatusCode:   status_code,
+			SpanId:       span_id,
+			ParentSpanId: parent_span_id,
+		}
+
+		curlist = append(curlist, dh)
+
+	}
+}
+
+func GetSpanId(r http.ResponseWriter) int64 {
+	mPtr := (r).(*responseTracer)
+	return mPtr.SpanId
 }
 
 // tracingResponseWriter implements a responsewriter with status
@@ -34,9 +50,16 @@ type tracingResponseWriter interface {
 }
 
 type responseTracer struct {
-	w      http.ResponseWriter
-	status int
-	size   int
+	w            http.ResponseWriter
+	status       int
+	size         int
+	SpanId       int64
+	ParentSpanId int64
+}
+
+func (l *responseTracer) newId() int64 {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Int63()
 }
 
 func (l *responseTracer) Header() http.Header {
@@ -81,26 +104,39 @@ func HTTPHandler(f func(w http.ResponseWriter, r *http.Request)) func(w http.Res
 				// hack
 				deferclient.Token = Token
 				deferclient.Prep(err)
-				appendHTTP(startTime, r.URL.Path, 500)
+				// FIXME
+				appendHTTP(startTime, r.URL.Path, 500, 0, 0)
 			}
 		}()
 
-		var tracer tracingResponseWriter
-		tracer = &responseTracer{w: w}
+		var tracer *responseTracer
+
+		tracer = &responseTracer{
+			w: w,
+		}
+
+		tracer.SpanId = tracer.newId()
+
+		r.ParseForm()
+		deferParentSpanId := r.FormValue("defer_parent_span_id")
+		if deferParentSpanId != "" {
+			log.Println("deferParentSpanId: [" + deferParentSpanId + "]")
+			tracer.ParentSpanId, _ = strconv.ParseInt(deferParentSpanId, 10, 64)
+		}
 
 		f(tracer, r)
 
-		appendHTTP(startTime, r.URL.Path, tracer.Status())
+		appendHTTP(startTime, r.URL.Path, tracer.Status(), tracer.SpanId, tracer.ParentSpanId)
 	}
 }
 
 // AddRequest allows external libraries to add a http request
-func AddRequest(start_time time.Time, path string, status_code int) {
+func AddRequest(start_time time.Time, path string, status_code int, span_id int64, parent_span_id int64) {
 
 	if Verbose {
 		log.Printf("Added manual request: %v\n", path)
 	}
 
 	// It's just an easier way to create third-party middlewares
-	appendHTTP(start_time, path, status_code)
+	appendHTTP(start_time, path, status_code, span_id, parent_span_id)
 }
