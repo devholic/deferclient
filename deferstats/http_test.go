5ca16c6969fb584870362dab183060c189eade49
Ian Eyberg <ian@deferpanic.com>
2015-04-30 11:17:54 -0700

    header test

--- deferstats/http_test.go
+++ deferstats/http_test.go
@@ -57,3 +57,44 @@ func TestHTTPPost(t *testing.T) {
 	defer resp.Body.Close()
 
 }
+
+func TestHTTPHeader(t *testing.T) {
+
+	dps := NewClient("token")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", dps.HTTPHandler(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-Custom-Header") != "some header" {
+			t.Error("headers not being passed through")
+		}
+
+		if r.Header.Get("Content-Type") != "application/json" {
+			t.Error("headers not being passed through")
+		}
+	}))
+
+	// set listener
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Error("http not listening")
+	}
+
+	dps.statsUrl = "http://" + l.Addr().String() + "/"
+
+	go http.Serve(l, mux)
+
+	url := "http://" + l.Addr().String() + "/"
+
+	var jsonStr = []byte(`{"Title":"sample title in json"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "some header")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+}
