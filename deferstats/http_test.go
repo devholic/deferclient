a3d2d9b365e26a6a0dc46102f69777b334db8a40
Ian Eyberg <ian@deferpanic.com>
2015-04-30 10:54:12 -0700

    POST body was being read and not passed

---
+++ deferstats/http_test.go
@@ -0,0 +1,59 @@
+package deferstats
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"testing"
+)
+
+type TestJSON struct {
+	Title string
+}
+
+func TestHTTPPost(t *testing.T) {
+
+	dps := NewClient("token")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", dps.HTTPHandler(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var tj TestJSON
+		err = json.Unmarshal(body, &tj)
+		if tj.Title != "sample title in json" {
+			t.Error("not parsing the POST body correctly")
+		}
+
+	}))
+
+	// set listener
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Error("http not listening")
+	}
+
+	dps.statsUrl = "http://" + l.Addr().String() + "/"
+
+	go http.Serve(l, mux)
+
+	url := "http://" + l.Addr().String() + "/"
+
+	var jsonStr = []byte(`{"Title":"sample title in json"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "some header")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+}
