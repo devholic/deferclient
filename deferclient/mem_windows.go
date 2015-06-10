b08abbb092084690fd39e490e7dadcda56ae622b
Ian Eyberg <ian@deferpanic.com>
2015-06-09 17:44:07 -0700

    windows stubs

---
+++ deferclient/mem_windows.go
@@ -0,0 +1,13 @@
+package deferclient
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// SetTotal returns the number of bytes on windows
+// currently just a stub
+// FIXME
+func (m *Mem) SetTotal() {
+	m.Total = 0
+}
---
+++ deferstats/filecnt_windows.go
@@ -0,0 +1,8 @@
+package deferstats
+
+// openFileCnt returns the number of open files in this process
+// currently just a stub
+// FIXME
+func openFileCnt() int {
+	return 0
+}
