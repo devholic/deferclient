1cf52c763c502e5b0073d6d8b9e42e11cadf0a28
Ian Eyberg <ian@deferpanic.com>
2015-07-13 10:18:09 -0700

    initial windows fc

--- deferstats/filecnt_windows.go
+++ deferstats/filecnt_windows.go
@@ -1,8 +1,27 @@
 package deferstats
 
+import (
+	"log"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	kernel32              = syscall.MustLoadDLL("kernel32.dll")
+	getProcessHandleCount = kernel32.MustFindProc("GetProcessHandleCount")
+)
+
 // openFileCnt returns the number of open files in this process
-// currently just a stub
-// FIXME
 func openFileCnt() int {
-	return 0
+	cp, err := syscall.GetCurrentProcess()
+	if err != nil {
+		log.Println("getcurrentProcess: %v\n", err)
+	}
+
+	var c uint32
+	r, _, err := getProcessHandleCount.Call(uintptr(cp), uintptr(unsafe.Pointer(&c)))
+	if r == 0 {
+		log.Println(err)
+	}
+	return int(c)
 }
